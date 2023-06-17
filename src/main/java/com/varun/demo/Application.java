45c16c333bba3ebd3eb478ce1feb159deae16a65
varunu28 <varun.u28@gmail.com>
2023-06-17 14:05:58 -0700

    Replaced in-memory datasource with postgres for jobrunr

--- src/main/java/com/varun/demo/Application.java
+++ src/main/java/com/varun/demo/Application.java
@@ -1,22 +1,47 @@
 package com.varun.demo;
 
 import org.jobrunr.jobs.mappers.JobMapper;
-import org.jobrunr.storage.InMemoryStorageProvider;
 import org.jobrunr.storage.StorageProvider;
+import org.jobrunr.storage.sql.postgres.PostgresStorageProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Application {
+
+	@Value("${spring.datasource.url}")
+	private String datasourceUrl;
+
+	@Value("${spring.datasource.password}")
+	private String datasourcePassword;
+
+	@Value("${spring.datasource.username}")
+	private String datasourceUsername;
+
+	private static final String POSTGRES_DRIVER_CLASS = "org.postgresql.Driver";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
-	public StorageProvider storageProvider(JobMapper jobMapper) {
-		InMemoryStorageProvider storageProvider = new InMemoryStorageProvider();
+	public DataSource postgresDataSource() {
+		return DataSourceBuilder.create()
+				.url(datasourceUrl)
+				.password(datasourcePassword)
+				.username(datasourceUsername)
+				.driverClassName(POSTGRES_DRIVER_CLASS)
+				.build();
+	}
+
+	@Bean
+	public StorageProvider storageProvider(JobMapper jobMapper, DataSource dataSource) {
+		StorageProvider storageProvider = new PostgresStorageProvider(dataSource);
 		storageProvider.setJobMapper(jobMapper);
 		return storageProvider;
 	}
